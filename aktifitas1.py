a036ff905df30be85c24dffd09113514e8249fe6
nissarestya <nissarestya@upi.edu>
2021-02-26 09:18:48 +0700

    Aktifitas 1

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("Nissa Restyasari: ")
+umur=20
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print("Nissa Restyasari: ", nama)
+print("20: ", umur)
+print("")
