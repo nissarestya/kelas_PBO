1c9b5f5fc7f691790195aaa77afe3c37fb697fa6
nissarestya <nissarestya@upi.edu>
2021-02-26 10:31:38 +0700

    xixi

---
+++ aktifitas4.py
@@ -0,0 +1,10 @@
+print("contoh perulangan suatu string")
+for huruf in "SISTEL":
+    print(huruf)
+    
+nilai_awal=0
+nilai_akhir=25 #tujuan/berapa kali
+step=5 #jeda antar loop
+print("contoh perulangan angka dengan range()")
+for i in range(nilai_awal, nilai_akhir, step):
+    print(i)
