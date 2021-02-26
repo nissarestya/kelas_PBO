282c79c2fc3507e5bc716d5536e9899e02b1ee48
nissarestya <nissarestya@upi.edu>
2021-02-26 10:24:23 +0700

    yeay bisaa

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("percabangan pada bahasa pemrograman Phyton")
+print("pilih bahasa Pemrograman: ")
+print("1. Phyton")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("bahasa pemrograman yang dipilih adalah C")
+else :
+    print("pilihan Anda tidak tersedia!!!")
